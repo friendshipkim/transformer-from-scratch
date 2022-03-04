7f1642397acd06f5e16b6320689e806edeccdc88
friendshipkim <kwj962004@gmail.com>
2022-03-03 23:15:11 -0500

    added test_encoder_layer.py, test_decoder_layer.py

---
+++ tests/test_encoder_layer.py
@@ -0,0 +1,23 @@
+import torch
+from model.layer.encoder_layer import EncoderLayer
+from config import *
+
+
+def test_encoder_layer():
+    # input - output of embedding layer
+    x = torch.rand((batch_size, max_seq_len, d_model))
+
+    encoder_layer = EncoderLayer(d_model=d_model, h=h, ffn_hidden=ffn_hidden, p_drop=p_drop)
+
+    # encoder mask
+    q_max_seq_len = k_max_seq_len = max_seq_len
+    mask = torch.randint(2, (batch_size, h, q_max_seq_len, k_max_seq_len))
+
+    # encoder layer output
+    out = encoder_layer(x, mask)
+
+    return out
+
+
+if __name__ == '__main__':
+    test_encoder_layer()
