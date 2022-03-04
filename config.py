8c37b1ed3566cc56a0234b0b44ff84a4ad5696b5
friendshipkim <kwj962004@gmail.com>
2022-03-04 13:17:35 -0500

    add device to config

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
+import torch
+
+# gpu device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# data hyperparameters
+vocab_size = 10000
+
 # model architecture hyperparameters
 n_layers = 6
-d_model = 512
+d_model = 256
 ffn_hidden = 2048
 h = 8
 p_drop = 0.1
-max_seq_len = 1000  # TODO: check paper
+max_seq_len = 100  # TODO: check paper
 
 # training hyperparameters
-batch_size = 32
+batch_size = 8
