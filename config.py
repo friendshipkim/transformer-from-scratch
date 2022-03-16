fix masking

config.py
@@ -4,15 +4,19 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # data hyperparameters
-vocab_size = 10000
+src_vocab_size = 10
+tgt_vocab_size = 15
+pad_idx = 1  # TODO
 
 # model architecture hyperparameters
 n_layers = 6
-d_model = 256
-ffn_hidden = 2048
+d_model = emb_size = 32
+ffn_hidden = d_model * 4
 h = 8
 p_drop = 0.1
-max_seq_len = 100  # TODO: check paper
+src_seq_len = 10
+tgt_seq_len = 12  # TODO: it differs by batch
 
 # training hyperparameters
-batch_size = 8
+batch_size = 2
+eval_batch_size = 16  # TODO: check if it is necessary

main.py
@@ -1,4 +1,6 @@
-import config  # hyperparameters
+import os.path
+
+import config as cfg  # hyperparameters
 from model.transformer import Transformer
 
 import torchtext.datasets
@@ -8,74 +10,178 @@ from torchtext.vocab import build_vocab_from_iterator
 from torch.utils.data import dataset
 import torch
 from torch import nn, Tensor
+from typing import Tuple
+import numpy as np
 
+import copy
+import time
+import math
+
+
+def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
+    """Converts raw text into a flat 1D Tensor."""
+    data = [
+        torch.tensor(vocab(tokenizer(item)), dtype=torch.long)
+        for item in raw_text_iter
+    ]
+    # vocab method maps tokens to indices.
+    return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))  # actually filtering isn't necessary
+    # TODO: why tuple - to use torch.cat, why there exist case t.numel() = 0 (blank line)
+    # line.strip() is empty, don't read that line
+
+def batchify(data: Tensor, batch_size: int) -> Tensor:
+    """Divides the data into batch_size separate sequences, removing extra elements
+    that wouldn't cleanly fit.
+
+    Args:
+        data: Tensor, shape [N]
+        batch_size: int, batch size
+
+    Returns:
+        Tensor of shape [N // bsz, bsz]
+    """
+    seq_len = data.size(0) // batch_size
+    data = data[: seq_len * batch_size]  # truncate data
+    data = data.view(batch_size, seq_len).t().contiguous()  # shape [seq_len, batch_size] # TODO: transpose??
+    return data.to(cfg.device)
+
+def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
+    """
+    Args:
+        source: Tensor, shape [full_seq_len, batch_size]
+        i: int
+
+    Returns:
+        tuple (data, target), where data has shape [max_seq_len, batch_size] and
+        target has shape [max_seq_len * batch_size]
+    """
+    seq_len = min(cfg.max_seq_len, len(source) - 1 - i)
+    # if shorter than max_seq_len, padding
+    if seq_len < cfg.max_seq_len:
+        print("sequence length is too short, padding:", seq_len)
+        return source[i:i + seq_len].t(), source[i:i + seq_len].t()
+        # padding = torch.ones(cfg.max_seq_len-seq_len, source.size(1)).to(cfg.device)
+        # # print(padding.shape)
+        # data = torch.cat((source[i:i + seq_len], padding), axis=0).long().t() # TODO: check: transposed
+        # print(data.shape)
+        # target = data.reshape(-1)
+        # return data, target
+
+    data = source[i:i + seq_len].t()  # shape: [max_seq_len, batch_size] TODO: why transpose this?
+    # target = data.reshape(-1)  # replication # shape: [max_seq_len * batch_size]
+    target = torch.fliplr(data).reshape(-1)  # reverse
+    # target = source[i + 1:i + 1 + seq_len].reshape(-1)  # language modeling # shape: [max_seq_len * batch_size]
+    return data, target
+
+def initialize_model(model):
+    # share embedding
+    # self.dec_embedding.weight = self.src_tok_emb.weight
+
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+    return model
+
+def train(model: nn.Module) -> None:
+    print("start training")
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 100
+    start_time = time.time()
+
+    num_batches = len(train_data) // cfg.max_seq_len
+    print("num_batches:", num_batches)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, cfg.max_seq_len)):
+        # print("batch", batch, "i", i)
+        data, targets = get_batch(train_data, i)
+        # truncate last batch
+        if data.size(1) < cfg.max_seq_len:
+            print("pass last batch")
+            continue
+        batch_size = data.size(0)
+        output = model(data, data)  # TODO: why one input
+        loss = criterion(output.view(-1, vocab_size), targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(model: nn.Module, eval_data: Tensor) -> float:
+    print("start testing")
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, cfg.max_seq_len):
+            data, targets = get_batch(eval_data, i)
+            # truncate last batch
+            if data.size(1) < cfg.max_seq_len:
+                print("pass last batch")
+                continue
+            batch_size = data.size(0)
+            output = model(data, data)
+            output_flat = output.view(-1, vocab_size)
+            total_loss += batch_size * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
+
+def count_parameters(model):
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    print("total # of params:", params)
 
 if __name__ == "__main__":
-    # use IWSLT dataset
-    # try smaller PTB dataset
+    # TODO: copy
+    # TODO: enc_input - src, dec_input - reverse src, target - shifted one reverse src (like translation task)
+    # TODO: use IWSLT dataset or (try smaller PTB dataset)
     # train, valid, test = datasets.PennTreebank(root='./data')
 
-    # preprocessing
-
-    # padding or cut the sequence
-    # build dataloader
-
-    # WikiText2
+    # Build vocab with WikiText2
     train_iter = WikiText2(split="train")
     tokenizer = get_tokenizer("basic_english")
     vocab = build_vocab_from_iterator(
-        map(
-            tokenizer, train_iter
-        ),  # TODO: what does it mean? map(tokenizer, train_iter)
-        specials=["<unk>", "<bos>", "<eos>"],
-    )  # we can add <bos>, <eos>
+        map(tokenizer, train_iter),  # apply tokenizer to train data to build vocab
+        specials=["<unk>", "<pad>", "<bos>", "<eos>"],  # we can add <bos>, <eos>, but language model doesn't need them
+        min_freq=5,  # control the size of vocab
+    )
     vocab.set_default_index(
         vocab["<unk>"]
     )  # This index will be returned when OOV token is queried
 
-    def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
-        """Converts raw text into a flat Tensor."""
-        data = [
-            torch.tensor(vocab(tokenizer(item)), dtype=torch.long)
-            for item in raw_text_iter
-        ]
-        # vocab method maps tokens to indices.
-        return torch.cat(
-            tuple(filter(lambda t: t.numel() > 0, data))
-        )  # TODO: why tuple?
+    # check pad token id
+    pad_idx = vocab["<pad>"]  # 1
 
+    vocab_size = len(vocab)
+
+    # Preprocess data - pad or cut the sequence
     # train_iter was "consumed" by the process of building the vocab,
     # so we have to create it again
     train_iter, val_iter, test_iter = WikiText2()
-    train_data = data_process(train_iter)
-    val_data = data_process(val_iter)
-    test_data = data_process(test_iter)
+    train_data = data_process(train_iter)  # shape: [2049990]
+    val_data = data_process(val_iter)  # shape: [214417]
+    test_data = data_process(test_iter)  # shape: [241859]
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # batchify data
+    train_data = batchify(train_data, cfg.batch_size)  # shape: [64062, 32]
+    val_data = batchify(val_data, cfg.eval_batch_size)  # shape: [13401, 16]
+    test_data = batchify(test_data, cfg.eval_batch_size)  # shape: [15116, 16]
 
-    def batchify(data: Tensor, bsz: int) -> Tensor:
-        """Divides the data into bsz separate sequences, removing extra elements
-        that wouldn't cleanly fit.
+    # from torch.utils.data import DataLoader
+    #
+    # train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    # test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
-        Args:
-            data: Tensor, shape [N]
-            bsz: int, batch size
-
-        Returns:
-            Tensor of shape [N // bsz, bsz]
-        """
-        seq_len = data.size(0) // bsz
-        data = data[: seq_len * bsz]
-        data = data.view(bsz, seq_len).t().contiguous()
-        return data.to(device)
-
-    batch_size = 20
-    eval_batch_size = 10
-    train_data_batch = batchify(train_data, batch_size)  # shape [seq_len, batch_size]
-    val_data_batch = batchify(val_data, eval_batch_size)
-    test_data_batch = batchify(test_data, eval_batch_size)
-
-    breakpoint()
 
     # build dataloader
     # we want input and target to be same
@@ -86,12 +192,75 @@ if __name__ == "__main__":
     # check if the model copy the unseen input
     # or reverse (change get_batch func)
 
-    model = Transformer(
-        n_layers=config.n_layers,
-        d_model=config.d_model,
-        h=config.h,
-        ffn_hidden=config.ffn_hidden,
-        p_drop=config.p_drop,
-        d_embed=config.d_embed,
-        vocab=vocab,
-    )
+    # define model
+    print("device:", cfg.device)
+
+    model = Transformer(n_layers=cfg.n_layers,
+                        d_model=cfg.d_model,
+                        h=cfg.h,
+                        ffn_hidden=cfg.ffn_hidden,
+                        p_drop=cfg.p_drop,
+                        vocab_size=vocab_size,
+                        max_seq_len=cfg.max_seq_len,
+                        pad_idx=pad_idx,
+                        device=cfg.device)
+
+    model.to(cfg.device)
+    print("parameter count:", count_parameters(model))
+
+    # Training
+    criterion = nn.CrossEntropyLoss()
+    lr = 5.0  # learning rate
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+
+    # test forward
+    test_src_input = get_batch(train_data, 0)[0].to(cfg.device)
+    test_tgt_input = test_src_input
+
+    forward_result = model(test_src_input, test_tgt_input)
+    # test if output shape is correct
+    assert forward_result.shape == (cfg.batch_size, cfg.max_seq_len, vocab_size)
+
+
+    best_val_loss = float('inf')
+    epochs = 3
+    best_model = None
+
+    # train model
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+        train(model)
+        val_loss = evaluate(model, val_data)
+        val_ppl = math.exp(val_loss)
+        elapsed = time.time() - epoch_start_time
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+              f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
+        print('-' * 89)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = copy.deepcopy(model)
+
+        scheduler.step()
+
+    # test model
+    test_loss = evaluate(best_model, test_data)
+    test_ppl = math.exp(test_loss)
+    print('=' * 89)
+    print(f'| End of training | test loss {test_loss:5.2f} | '
+          f'test ppl {test_ppl:8.2f}')
+    print('=' * 89)
+
+    # save model
+    MODEL_PATH = './checkpoint/reverse.pth'
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        # 'loss': loss
+    }, MODEL_PATH)
+
+    breakpoint()

model/encoder.py
@@ -10,6 +10,7 @@ class Encoder(nn.Module):
         self.layers = nn.ModuleList(
             [EncoderLayer(d_model, h, ffn_hidden, p_drop) for _ in range(n_layers)]
         )
+        self.norm = nn.LayerNorm(d_model)  # TODO: layernorm at last?
 
     def forward(self, x: Tensor, enc_mask: Tensor) -> Tensor:
         """
@@ -18,6 +19,7 @@ class Encoder(nn.Module):
 
         :return: torch.Tensor, shape: (batch_size, max_seq_len, d_model)
         """
-        for layer in self.layers:
+        for i, layer in enumerate(self.layers):
             x = layer(x, enc_mask)
+        x = self.norm(x)
         return x

model/layer/decoder_layer.py
@@ -7,17 +7,17 @@ from model.sublayer.pointwise_feedforward import PointwiseFeedForward
 class DecoderLayer(nn.Module):
     def __init__(self, d_model: int, h: int, ffn_hidden: int, p_drop: float):
         super(DecoderLayer, self).__init__()
-        self.attn_self = MultiHeadAttention(d_model, h)
-        self.norm1 = nn.LayerNorm(d_model)
+        self.self_attn = MultiHeadAttention(d_model, h)
         self.dropout1 = nn.Dropout(p_drop)
+        self.norm1 = nn.LayerNorm(d_model)
 
-        self.attn_cross = MultiHeadAttention(d_model, h)
-        self.norm2 = nn.LayerNorm(d_model)
+        self.cross_attn = MultiHeadAttention(d_model, h)
         self.dropout2 = nn.Dropout(p_drop)
+        self.norm2 = nn.LayerNorm(d_model)
 
-        self.ffn = PointwiseFeedForward(d_model, ffn_hidden)
-        self.norm3 = nn.LayerNorm(d_model)
+        self.ffn = PointwiseFeedForward(d_model, ffn_hidden, p_drop)
         self.dropout3 = nn.Dropout(p_drop)
+        self.norm3 = nn.LayerNorm(d_model)
 
     def forward(
         self, x: Tensor, enc_output: Tensor, dec_mask: Tensor, cross_mask: Tensor
@@ -32,20 +32,20 @@ class DecoderLayer(nn.Module):
         """
         # 1. self-attention
         x_cp = x
-        x = self.attn_self(q=x, k=x, v=x, mask=dec_mask)["output"]
-        x = self.norm1(x + x_cp)
+        x = self.self_attn(q=x, k=x, v=x, mask=dec_mask)["output"]
         x = self.dropout1(x)
+        x = self.norm1(x + x_cp)
 
         # 2. cross-attention
         x_cp = x
-        x = self.attn_cross(q=x, k=enc_output, v=enc_output, mask=cross_mask)["output"]
-        x = self.norm2(x + x_cp)
+        x = self.cross_attn(q=x, k=enc_output, v=enc_output, mask=cross_mask)["output"]
         x = self.dropout2(x)
+        x = self.norm2(x + x_cp)
 
         # 2. feed forward
         x_cp = x
         x = self.ffn(x)
-        x = self.norm3(x + x_cp)
         x = self.dropout3(x)
+        x = self.norm3(x + x_cp)
 
         return x

model/layer/embedding.py
@@ -1,13 +1,16 @@
-import torch
-import math
+# TODO: Not necessary
+
 from torch import nn, Tensor
+
+from model.sublayer.token_embedding import TokenEmbedding
+from model.sublayer.positional_encoding import PositionalEncoding
 
 
 class Embedding(nn.Module):
     def __init__(self, d_model: int, vocab_size: int, max_seq_len: int):
         super(Embedding, self).__init__()
         self.token_embedding = TokenEmbedding(d_model, vocab_size)
-        self.positional_encoding = PositionalEncoding(d_model, max_seq_len)
+        self.positional_encoding = PositionalEncoding(d_model, )
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -19,55 +22,4 @@ class Embedding(nn.Module):
         return out
 
 
-class TokenEmbedding(nn.Module):
-    def __init__(self, d_model: int, vocab_size: int):
-        super(TokenEmbedding, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, d_model)
-        self.d_model = d_model
 
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        :param x: torch.Tensor, shape: (batch_size, max_seq_len)
-        :return: torch.Tensor, shape: (batch_size, max_seq_len, d_model)
-        """
-        out = self.embedding(x) * math.sqrt(
-            self.d_model
-        )  # multiply embedding weights by sqrt(d_model)
-        return out
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, max_seq_len: int):
-        super(PositionalEncoding, self).__init__()
-        encoding = torch.zeros(max_seq_len, d_model)
-        position = torch.arange(0, max_seq_len)[
-            ..., None
-        ]  # [max_seq_len, 1], use [..., None] instead of .unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
-        )  # log space
-        # div_term = torch.reciprocal(torch.pow(10000, torch.arange(0, d_model, 2) / d_model))
-        encoding[:, 0::2] = torch.sin(
-            position * div_term
-        )  # position * div_term broadcasted to [max_seq_len, d_model/2]
-        encoding[:, 1::2] = torch.cos(position * div_term)
-
-        # my implementation
-        # position = torch.arange(0, max_seq_len)[..., None]
-        # sinusoid_input = position / (10000 ** torch.arange(0, d_model, 2) / d_model)
-        # encoding[:, 0::2] = torch.sin(sinusoid_input)
-        # encoding[:, 1::2] = torch.cos(sinusoid_input)
-
-        self.encoding = encoding
-
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        :param x: torch.Tensor, shape: (batch_size, max_seq_len, d_model)
-        :return: torch.Tensor, shape: (batch_size, max_seq_len, d_model)
-        """
-        seq_len = x.size(1)
-        # TODO: why do we need seq_length besides max_seq_length?
-        # TODO: do we have to use torch.autograd.Variable?, self.encoding already requires_grad=False
-        pe = torch.autograd.Variable(self.encoding[:seq_len, :], requires_grad=False)
-        x = x + pe
-        return x  # x = x + self.encoding[:seq_len, :]

model/layer/encoder_layer.py
@@ -1,20 +1,24 @@
 from torch import nn
 from torch import Tensor
 
+import torch
+
 from model.sublayer.multihead_attention import MultiHeadAttention
 from model.sublayer.pointwise_feedforward import PointwiseFeedForward
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, d_model: int, h: int, ffn_hidden: int, p_drop: int):
+    def __init__(self, d_model: int, h: int, ffn_hidden: int, p_drop: float):
         super(EncoderLayer, self).__init__()
-        self.attn = MultiHeadAttention(d_model, h)
+        self.self_attn = MultiHeadAttention(d_model, h)
         self.norm1 = nn.LayerNorm(d_model)
         self.dropout1 = nn.Dropout(p_drop)
 
-        self.ffn = PointwiseFeedForward(d_model, ffn_hidden)
+        self.ffn = PointwiseFeedForward(d_model, ffn_hidden, p_drop)
         self.norm2 = nn.LayerNorm(d_model)
         self.dropout2 = nn.Dropout(p_drop)
+
+        self.attn_out_weights = None
 
     def forward(self, x: Tensor, enc_mask: Tensor = None) -> Tensor:
         """
@@ -24,16 +28,24 @@ class EncoderLayer(nn.Module):
         """
 
         # 1. attention
-        x_out_dict = self.attn(q=x, k=x, v=x, mask=enc_mask)
-        x_out = x_out_dict["output"]
+        attn_out_dict = self.self_attn(q=x, k=x, v=x, mask=enc_mask)
+        attn_out = attn_out_dict["output"]
+        attn_out_weights = attn_out_dict["attn_score"]
+        out = self.dropout1(attn_out)
+        out = self.norm1(x + out)
 
-        x_out = self.norm1(x + x_out)
-        x_out = self.dropout1(x_out)
+        self.attn_out_weights = attn_out_weights
+
+        # torch.save(x, f'/home/wk247/workspace/transformer-from-scratch/tensors/my.encoder.{i}.selfattn.norm.pt')
+        # print(f'saving mine - {i}th layer after selfattn')
 
         # 2. feed forward
-        cp_x_out = x_out
-        x_out = self.ffn(x_out)
-        x_out = self.norm2(x_out + cp_x_out)
-        x_out = self.dropout2(x_out)
+        cp_out = out
+        out = self.ffn(out)
+        out = self.dropout2(out)
+        out = self.norm2(out + cp_out)
 
-        return x_out
+        # torch.save(x, f'/home/wk247/workspace/transformer-from-scratch/tensors/my.encoder.{i}.ffn.norm.pt')
+        # print(f'saving mine - {i}th layer after ffn')
+
+        return out

model/sublayer/multihead_attention.py
@@ -16,7 +16,7 @@ class MultiHeadAttention(nn.Module):
         self.fc_concat = nn.Linear(d_model, d_model)
 
     def forward(
-        self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor) -> typing.Dict[str, Tensor]:
+            self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor = None) -> typing.Dict[str, Tensor]:
         """
         :param q: torch.Tensor, shape: (batch_size, q_max_seq_len, d_model)
         :param k: torch.Tensor, shape: (batch_size, k_max_seq_len, d_model)
@@ -68,7 +68,7 @@ class MultiHeadAttention(nn.Module):
         )
 
     def calculate_attn(
-        self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor
+            self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor = None
     ) -> typing.Dict[str, Tensor]:
         """
         calculate scaled dot product attention
@@ -91,9 +91,19 @@ class MultiHeadAttention(nn.Module):
 
         # (3. masking)
         if mask is not None:
+            print("do masking")
+            # mask = torch.zeros_like(mask, dtype=torch.float)  # all zeros
+            # mask.masked_fill_(mask, float("-inf"))
+            # mask.attn
+
             # given mask with shape (q_max_seq_len, k_max_seq_len),
             # it is broadcasted to (batch_size, h, q_max_seq_len, k_max_seq_len)
-            attn_score = attn_score.masked_fill(mask == 0, value=float("-Inf"))
+            print(attn_score.shape)
+            attn_score = attn_score.masked_fill(mask.unsqueeze(1) == 0, value=float("-inf"))
+            # print(attn_score)
+            # print(attn_score.shape)
+            # print(mask.shape)
+            # print(mask)
 
         # 4. softmax
         attn_score = attn_score.softmax(

model/sublayer/pointwise_feedforward.py
@@ -3,7 +3,7 @@ from torch import Tensor
 
 
 class PointwiseFeedForward(nn.Module):
-    def __init__(self, d_model: int, ffn_hidden: int):
+    def __init__(self, d_model: int, ffn_hidden: int, p_drop: float):
         super(PointwiseFeedForward, self).__init__()
         self.d_model = d_model
         self.ffn_hidden = ffn_hidden
@@ -12,6 +12,8 @@ class PointwiseFeedForward(nn.Module):
         self.relu = nn.ReLU()
         self.fc_2 = nn.Linear(ffn_hidden, d_model)
 
+        self.dropout = nn.Dropout(p_drop)
+
     def forward(self, x: Tensor) -> Tensor:
         """
         :param x: torch.Tensor, shape: (batch_size, max_seq_len, d_model)
@@ -19,6 +21,7 @@ class PointwiseFeedForward(nn.Module):
         """
         x = self.fc_1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc_2(x)
 
         return x

model/transformer.py
@@ -3,7 +3,8 @@ import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
 
-from model.layer.embedding import Embedding
+from model.sublayer.token_embedding import TokenEmbedding
+from model.sublayer.positional_encoding import PositionalEncoding
 from model.encoder import Encoder
 from model.decoder import Decoder
 
@@ -16,20 +17,20 @@ class Transformer(nn.Module):
             h: int,
             ffn_hidden: int,
             p_drop: float,
-            vocab_size: int,
-            max_seq_len: int,
-            pad_idx: int,  # TODO: check if source, target use same pad_idx
+            src_vocab_size: int,
+            tgt_vocab_size: int,
+            pad_idx: int,
             device: str
     ):
         super(Transformer, self).__init__()
 
         self.pad_idx = pad_idx
-        self.h = h  # TODO: remove to use broadcasting
+        self.device = device
+        # self.h = h  # TODO: remove not to use broadcasting
 
         # encoder
-        self.enc_embedding = Embedding(
-            d_model=d_model, vocab_size=vocab_size, max_seq_len=max_seq_len
-        )
+        self.src_tok_emb = TokenEmbedding(emb_size=d_model, vocab_size=src_vocab_size)
+        self.positional_encoding = PositionalEncoding(emb_size=d_model, p_drop=p_drop)  # TODO: dropout necessary?
         self.encoder = Encoder(
             n_layers=n_layers,
             d_model=d_model,
@@ -39,9 +40,11 @@ class Transformer(nn.Module):
         )
 
         # decoder
-        self.dec_embedding = self.enc_embedding
-        # TODO: how to make shared embedding, what if source/target vocab size is different?
-        # self.dec_embedding = Embedding(d_model=d_model, vocab_size=vocab_size, max_seq_len=max_seq_len)
+        self.tgt_tok_emb = TokenEmbedding(emb_size=d_model, vocab_size=tgt_vocab_size)
+        # how to make shared embedding
+        # 1. assign same weight when initialization
+        # 2. self.src_tok_emb = self.tgt_tok_emb
+        # what if source/target vocab size is different?
         self.decoder = Decoder(
             n_layers=n_layers,
             d_model=d_model,
@@ -51,51 +54,44 @@ class Transformer(nn.Module):
         )
 
         # final classifier
-        self.classifier = nn.Linear(d_model, vocab_size)
+        self.classifier = nn.Linear(d_model, tgt_vocab_size)
 
-    def forward(self, enc_input: Tensor, dec_input: Tensor) -> Tensor:
+    def forward(self, src: Tensor, tgt: Tensor) -> Tensor:
         """
-        :param enc_input: torch.Tensor, shape: (batch_size, max_seq_len)
-        :param dec_input: torch.Tensor, shape: (batch_size, max_seq_len)
+        :param src: torch.Tensor, shape: (batch_size, src_seq_len)
+        :param tgt: torch.Tensor, shape: (batch_size, tgt_seq_len)
 
-        :return: torch.Tensor, shape: (batch_size, max_seq_len, vocab_size)
+        :return: torch.Tensor, shape: (batch_size, tgt_seq_len, tgt_vocab_size)
         """
-
-        # masks
-        enc_mask = self.make_pad_mask(enc_input, enc_input, self.pad_idx)
-        print("enc_mask shape:", enc_mask.shape)
-        cross_mask = self.make_pad_mask(dec_input, enc_input, self.pad_idx)
-        print("cross_mask shape:", cross_mask.shape)
-        dec_mask = self.make_pad_mask(dec_input, dec_input, self.pad_idx) & \
-                   self.make_autoregressive_mask(dec_input, dec_input)  # broadcasted TODO: check
-        print("dec_mask shape:", dec_mask.shape)
+        # mask
+        src_mask, cross_mask, _, _, tgt_mask = self.create_mask(src, tgt)
 
         # encoder
-        enc_embedding = self.enc_embedding(x=enc_input)
-        enc_output = self.encoder(x=enc_embedding, enc_mask=enc_mask)  # context
+        enc_embedding = self.positional_encoding(self.src_tok_emb(src))
+        enc_output = self.encoder(x=enc_embedding, enc_mask=src_mask)  # memory
 
         # decoder
-        dec_embedding = self.dec_embedding(x=dec_input)
-        dec_out = self.decoder(
+        dec_embedding = self.positional_encoding(self.tgt_tok_emb(tgt))
+        dec_output = self.decoder(
             x=dec_embedding,
             enc_output=enc_output,
-            dec_mask=dec_mask,
+            dec_mask=tgt_mask,
             cross_mask=cross_mask,
         )
 
         # final classifier
-        model_out = self.classifier(dec_out)
-        return model_out  # F.log_softmax(model_out, dim=-1) # TODO
+        model_out = self.classifier(dec_output)
+        return enc_embedding, enc_output, dec_embedding, dec_output, model_out  # F.log_softmax(model_out, dim=-1) # TODO
 
-    def make_pad_mask(self, q: Tensor, k: Tensor, pad_idx: int) -> Tensor:
+    def create_pad_mask(self, q: Tensor, k: Tensor, pad_idx: int) -> Tensor:
         """
         Create a mask to hide padding
 
-        :param q: torch.Tensor, shape: (batch_size, q_max_seq_len)
-        :param k: torch.Tensor, shape: (batch_size, k_max_seq_len)
+        :param q: torch.Tensor, shape: (batch_size, q_seq_len)
+        :param k: torch.Tensor, shape: (batch_size, k_seq_len)
         :param pad_idx: int, pad token index
 
-        :return: torch.Tensor, shape: (batch_size, 1, q_max_seq_len, k_max_seq_len)
+        :return: torch.Tensor, shape: (batch_size, 1, q_seq_len, k_seq_len)
         """
         len_q, len_k = q.size(1), k.size(1)
 
@@ -110,19 +106,37 @@ class Transformer(nn.Module):
         q = q.repeat(1, 1, 1, len_k)
 
         mask = k & q
-        return mask  # TODO: it can be also broadcasted
+        return mask.to(self.device)  # shape: (batch_size, 1, q_seq_len, k_seq_len) it can be also broadcasted
 
-    def make_autoregressive_mask(self, q: Tensor, k: Tensor) -> Tensor:
+    def create_autoregressive_mask(self, q: Tensor, k: Tensor) -> Tensor:  # TODO: src, tgt
         """
-        :param q: torch.Tensor, shape: (batch_size, q_max_seq_len)
-        :param k: torch.Tensor, shape: (batch_size, k_max_seq_len)
-        :return: torch.Tensor, shape: (batch_size, h, q_max_seq_len, k_max_seq_len)
+        :param q: torch.Tensor, shape: (batch_size, q_seq_len)
+        :param k: torch.Tensor, shape: (batch_size, k_seq_len)
+        :return: torch.Tensor, shape: (batch_size, q_seq_len, k_seq_len)
         """
         batch_size = q.size(0)
         len_q, len_k = q.size(1), k.size(1)
-        mask = torch.ones(len_q, len_k, dtype=torch.bool).tril(diagonal=0)
+        mask = torch.ones(len_q, len_k, dtype=torch.bool).tril(diagonal=0)  # TODO: check if triu is correct
 
         # we can just return here, it will be broadcasted anyway
         # now let's stack it just for shape consistency
-        mask = mask.repeat(batch_size, self.h, 1, 1)
-        return mask
+        # mask = mask.repeat(batch_size, self.h, 1, 1)
+        return mask.to(self.device)
+
+    def create_mask(self, src, tgt):
+        # masks
+        src_mask = self.create_pad_mask(src, src, self.pad_idx)
+        # print("src_mask shape:", src_mask.shape)
+        cross_mask = self.create_pad_mask(tgt, src, self.pad_idx)
+        # print("cross_mask shape:", cross_mask.shape)
+
+        tgt_pad_mask = self.create_pad_mask(tgt, tgt, self.pad_idx)
+        tgt_autoregressive_mask = self.create_autoregressive_mask(tgt, tgt)
+        # print("tgt_pad_mask shape:", tgt_pad_mask)
+        # print("tgt_autoregressive_mask shape:", tgt_autoregressive_mask)
+
+        tgt_mask = tgt_pad_mask & tgt_autoregressive_mask  # broadcasted TODO: check
+        # print("tgt_mask shape:", tgt_mask)
+
+        return src_mask, cross_mask, tgt_pad_mask, tgt_autoregressive_mask, tgt_mask
+        # return src_mask, cross_mask, tgt_mask