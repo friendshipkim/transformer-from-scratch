cfef0e9f114eead113b46b45d6df43e2b01ac26b
friendshipkim <kwj962004@gmail.com>
2022-03-21 16:44:08 -0400

    added comments: TBD

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
     # TODO: why tuple - to use torch.cat, why there exist case t.numel() = 0 (blank line)
     # line.strip() is empty, don't read that line
 
+
 def batchify(data: Tensor, batch_size: int) -> Tensor:
     """Divides the data into batch_size separate sequences, removing extra elements
     that wouldn't cleanly fit.
@@ -45,6 +46,7 @@ def batchify(data: Tensor, batch_size: int) -> Tensor:
     data = data.view(batch_size, seq_len).t().contiguous()  # shape [seq_len, batch_size] # TODO: transpose??
     return data.to(cfg.device)
 
+
 def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
     """
     Args:
@@ -55,9 +57,9 @@ def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
         tuple (data, target), where data has shape [max_seq_len, batch_size] and
         target has shape [max_seq_len * batch_size]
     """
-    seq_len = min(cfg.max_seq_len, len(source) - 1 - i)
+    seq_len = min(cfg.src_seq_len, len(source) - 1 - i)
     # if shorter than max_seq_len, padding
-    if seq_len < cfg.max_seq_len:
+    if seq_len < cfg.src_seq_len:
         print("sequence length is too short, padding:", seq_len)
         return source[i:i + seq_len].t(), source[i:i + seq_len].t()
         # padding = torch.ones(cfg.max_seq_len-seq_len, source.size(1)).to(cfg.device)
@@ -73,6 +75,7 @@ def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
     # target = source[i + 1:i + 1 + seq_len].reshape(-1)  # language modeling # shape: [max_seq_len * batch_size]
     return data, target
 
+
 def initialize_model(model):
     # share embedding
     # self.dec_embedding.weight = self.src_tok_emb.weight
@@ -82,6 +85,7 @@ def initialize_model(model):
             nn.init.xavier_uniform(p)
     return model
 
+
 def train(model: nn.Module) -> None:
     print("start training")
     model.train()  # turn on train mode
@@ -89,17 +93,20 @@ def train(model: nn.Module) -> None:
     log_interval = 100
     start_time = time.time()
 
-    num_batches = len(train_data) // cfg.max_seq_len
+    num_batches = len(train_data) // cfg.src_seq_len
     print("num_batches:", num_batches)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, cfg.max_seq_len)):
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, cfg.src_seq_len)):
         # print("batch", batch, "i", i)
         data, targets = get_batch(train_data, i)
         # truncate last batch
-        if data.size(1) < cfg.max_seq_len:
+        if data.size(1) < cfg.src_seq_len:
             print("pass last batch")
             continue
         batch_size = data.size(0)
-        output = model(data, data)  # TODO: why one input
+        output = model(data, data)[-1]
+        breakpoint()
+        print("output shape", output.view(-1, vocab_size).shape)  # TODO: why one input
+        print("target shape", targets.shape)
         loss = criterion(output.view(-1, vocab_size), targets)
 
         optimizer.zero_grad()
@@ -119,6 +126,7 @@ def train(model: nn.Module) -> None:
             total_loss = 0
             start_time = time.time()
 
+
 def evaluate(model: nn.Module, eval_data: Tensor) -> float:
     print("start testing")
     model.eval()  # turn on evaluation mode
@@ -136,10 +144,12 @@ def evaluate(model: nn.Module, eval_data: Tensor) -> float:
             total_loss += batch_size * criterion(output_flat, targets).item()
     return total_loss / (len(eval_data) - 1)
 
+
 def count_parameters(model):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
     print("total # of params:", params)
+
 
 if __name__ == "__main__":
     # TODO: copy
@@ -182,7 +192,6 @@ if __name__ == "__main__":
     # train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
     # test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
-
     # build dataloader
     # we want input and target to be same
     # chunk the train data batch with batch size torch.chunk(train_data_batch, batch_size)
@@ -191,7 +200,6 @@ if __name__ == "__main__":
 
     # check if the model copy the unseen input
     # or reverse (change get_batch func)
-
     # define model
     print("device:", cfg.device)
 
@@ -200,11 +208,10 @@ if __name__ == "__main__":
                         h=cfg.h,
                         ffn_hidden=cfg.ffn_hidden,
                         p_drop=cfg.p_drop,
-                        vocab_size=vocab_size,
-                        max_seq_len=cfg.max_seq_len,
-                        pad_idx=pad_idx,
+                        src_vocab_size=vocab_size,
+                        tgt_vocab_size=vocab_size,
+                        pad_idx=cfg.pad_idx,
                         device=cfg.device)
-
     model.to(cfg.device)
     print("parameter count:", count_parameters(model))
 
@@ -214,15 +221,13 @@ if __name__ == "__main__":
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 
-
     # test forward
     test_src_input = get_batch(train_data, 0)[0].to(cfg.device)
     test_tgt_input = test_src_input
 
-    forward_result = model(test_src_input, test_tgt_input)
+    forward_result = model(test_src_input, test_tgt_input)[-1]
     # test if output shape is correct
-    assert forward_result.shape == (cfg.batch_size, cfg.max_seq_len, vocab_size)
-
+    assert forward_result.shape == (cfg.batch_size, cfg.src_seq_len, vocab_size)
 
     best_val_loss = float('inf')
     epochs = 3
