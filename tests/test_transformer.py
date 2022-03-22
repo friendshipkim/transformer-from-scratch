68084f87581bdab9b581af1b101d42bf0ac0ba94
friendshipkim <kwj962004@gmail.com>
2022-03-22 12:58:59 -0400

    change training code

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -133,7 +133,7 @@ def test_transformer():
 
     # check if gradients are the same
     check_transformer_grads(baseline_model, my_model)
-    # breakpoint()
+    breakpoint()
 
 if __name__ == "__main__":
     test_transformer()
