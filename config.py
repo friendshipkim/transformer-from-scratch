b209363556cf04cac83770c9bdfe33546698d9f3
friendshipkim <kwj962004@gmail.com>
2022-03-31 00:55:55 -0400

    update config

--- config.py
+++ config.py
@@ -4,10 +4,12 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 seed = 0
 
-# data hyperparameters
+# data hyperparameters (just for testing)
 src_vocab_size = 10000
 tgt_vocab_size = 15000
 pad_idx = 1
+src_seq_len = 100  # it differs by batch
+tgt_seq_len = 120  # it differs by batch
 
 # model architecture hyperparameters
 n_layers = 6
@@ -15,9 +17,8 @@ d_model = emb_size = 512
 ffn_hidden = d_model * 4
 h = 8
 p_drop = 0.1
-src_seq_len = 100  # it differs by batch
-tgt_seq_len = 120  # it differs by batch
 
 # training hyperparameters
 batch_size = 32
-eval_batch_size = 16  # TODO: check if it is necessary
+epochs = 15
+lr = 0.0001
